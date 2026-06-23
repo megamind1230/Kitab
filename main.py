fdfe50668330e444d4b6efc6de6ac112f5f6b5ba
abdulrahman-103 <176532798+abdulrahman-103@users.noreply.github.com>
2026-06-23 13:43:46 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,16 @@ from PySide6.QtWidgets import QApplication
 from mainwindow import MainWindow
 import sys
 
+
+#minimize terminal remove later
+import os
+if sys.platform == "win32":
+    import ctypes
+    console_window = ctypes.windll.kernel32.GetConsoleWindow()
+    if console_window != 0:
+        ctypes.windll.user32.ShowWindow(console_window, 0)
+
+
 app = QApplication(sys.argv)
 screen_resolution = app.primaryScreen().availableSize()
 window = MainWindow()
